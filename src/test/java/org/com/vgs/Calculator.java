549c685590c6ea752a25851680c8b651de8cd590
sravanthivemuganti <50642616+sravanthivemuganti@users.noreply.github.com>
2019-09-30 17:21:09 -0700

    Update Calculator.java

--- src/test/java/org/com/vgs/Calculator.java
+++ src/test/java/org/com/vgs/Calculator.java
@@ -7,6 +7,7 @@ public class Calculator {
 	}
 	public static void mul(int a,int b){
 		int c=a*b;
+		System.out.println("Multipilication of: "+c);
 	}
 	public static void add(int a,int b) {
 		int c=a+b;
