4d1c80519e6b47cff06aea15df9f480d79f301b4
sravanthi <vgs.godha@gmail.com>
2019-09-30 17:11:34 -0700

    Adding project to git

---
+++ src/main/java/org/com/vgs/App.java
@@ -0,0 +1,13 @@
+package org.com.vgs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/com/vgs/Calculator.java
@@ -0,0 +1,12 @@
+package org.com.vgs;
+
+public class Calculator {
+	public static void main(String[]args) {
+		int a=10,b=5;	
+		add(a,b);
+	}
+	public static void add(int a,int b) {
+		int c=a+b;
+		System.out.println(c);
+	}
+}
